2c6209588b6c99d6026c6585c7c3d19fee7e612d
Stephanie Caston <Stephanie.Caston@agdata.com>
2025-08-11 16:00:49 -0400

    Created container deploy script

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     module:{
         rules: [
@@ -9,9 +10,14 @@ module.exports = {
                     options: {
                         presets: ['@babel/preset-react','@babel/preset-env'],
                         plugins: ['@babel/plugin-transform-runtime']
-                    }
-                }
-            }
-        ]
-    }
-}
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+         new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+    ],
+};
