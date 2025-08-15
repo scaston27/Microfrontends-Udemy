041a1609dd18f815586d489abfe8741f25949b63
Stephanie Caston <Stephanie.Caston@agdata.com>
2025-08-15 17:10:17 -0400

    Added navigation logic

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history'; 
 import App from './App'
 
-const mount = (el) => {
-    ReactDOM.render(<App />,el);
-};
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
 
+    if(onNavigate){
+        history.listen(onNavigate);
+    }
+
+    ReactDOM.render(<App history={history} />,el);
+
+    return {
+        onParentNavigate({pathname: nextPathName}) {
+            const {pathname} = history.location;
+            if(pathname !== nextPathName){
+                history.push(nextPathName); 
+            }
+        }
+    };
+};
 
 if (process.env.NODE_ENV === 'development'){
     const devRoot = document.querySelector('#_marketing-dev-root');
 
     if (devRoot){
-        mount(devRoot); 
+        mount(devRoot, { defaultHistory: createBrowserHistory() }); 
     }
 }
 
